7d1335832ed17173988a4fb6f55ba4fa6b1253b4
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2024-01-11 05:50:56 +0000

    impl loop

--- src/main.rs
+++ src/main.rs
@@ -12,35 +12,37 @@ fn main() {
     let mut map = HashMap::<u64, Node>::new();
     let mut g_set = HashSet::<i64>::new();
 
-    println!("input operation(add, lookup)");
+    loop {
+        println!("input operation(add, lookup)");
 
-    // 標準入力からの操作を受け付ける
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let input = input.trim();
+        // 標準入力からの操作を受け付ける
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
 
-    // 操作が"add"だった場合
-    if input == "add" {
-        // 標準入力からの値を受け付ける
-        let mut value = String::new();
-        println!("input number");
-        std::io::stdin().read_line(&mut value).unwrap();
-        let value = value.trim().parse::<i64>().unwrap();
+        // 操作が"add"だった場合
+        if input == "add" {
+            // 標準入力からの値を受け付ける
+            let mut value = String::new();
+            println!("input number");
+            std::io::stdin().read_line(&mut value).unwrap();
+            let value = value.trim().parse::<i64>().unwrap();
 
-        // Nodeを作成
-        let node = Node::new(("add".to_string(), value));
-        println!("node: {:?}", node);
+            // Nodeを作成
+            let node = Node::new(("add".to_string(), value));
+            println!("node: {:?}", node);
 
-        // GraphにNodeのCIDを追加
-        graph.add_node(node.cid.clone());
+            // GraphにNodeのCIDを追加
+            graph.add_node(node.cid.clone());
 
-        // HashMapにNodeを追加
-        // これをすることによりCIDからNodeを引くことができるようになる
-        map.insert(graph.get_nodes_len().try_into().unwrap(), node);
-        println!("graph: {:?}", graph);
+            // HashMapにNodeを追加
+            // これをすることによりCIDからNodeを引くことができるようになる
+            map.insert(graph.get_nodes_len().try_into().unwrap(), node);
+            println!("graph: {:?}", graph);
 
-        // g-setにValueを追加
-        g_set.insert(value);
-        println!("g_set: {:?}", g_set);
+            // g-setにValueを追加
+            g_set.insert(value);
+            println!("g_set: {:?}", g_set);
+        }
     }
 }
