5f6f7ae6196bbc371cf49de5e77c9571ca1b7d39
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2024-01-21 06:27:01 +0000

    add_nodeを修正
    重複ノードの場合は追加しないように修正

--- src/graph.rs
+++ src/graph.rs
@@ -10,6 +10,10 @@ impl Graph {
         Graph { nodes: Vec::new() }
     }
     pub fn add_node(&mut self, cid: Cid) {
+        // 重複ノードの場合は追加しない
+        if self.search_node(&cid).is_some() {
+            return;
+        }
         self.nodes.push(cid);
     }
     pub fn pop_node(&mut self) -> Option<Cid> {
@@ -17,5 +21,8 @@ impl Graph {
     }
     pub fn get_nodes(&self) -> &Vec<Cid> {
         &self.nodes
+    }
+    fn search_node(&self, cid: &Cid) -> Option<usize> {
+        self.nodes.iter().position(|x| x == cid)
     }
 }
